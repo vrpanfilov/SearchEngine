bc6ac2c08db7e589ed994b3d50f1335c9490c28b
Vladimir Panfilov <vrpanfilov@yandex.ru>
2022-10-15 15:35:10 +0300

    Simplified the connection of morphology.

--- init_morphology.cmd
+++
@@ -1,6 +0,0 @@
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=.\morphology\dictionary-reader.jar &^
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=.\morphology\english.jar &^
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=.\morphology\morph.jar &^
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=.\morphology\morphology.jar &^
-mvn org.apache.maven.plugins:maven-install-plugin:2.5.2:install-file -Dfile=.\morphology\russian.jar
-
